bd1ed181bd84a1eda2a5b58bd1d393d944ade2f2
nithin3003 <msnithin84@gmail.com>
2024-08-10 13:03:19 +0000

    hi

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from flask import Flask,redirect,url_for,render_template,request,session
 # from werkzeug.security import generate_password_hash, check_password_hash
 from secrets import token_urlsafe
 import os
-import google.generativeai as genai
+# import google.generativeai as genai
 from datetime import datetime
+import vertexai
+from vertexai.preview.generative_models import GenerativeModel, ChatSession
+
 
 
 app= Flask(__name__) 
@@ -17,8 +20,8 @@ app.config['SECRET_KEY'] = token_urlsafe(32)
 mongo = PyMongo(app)
 coded = mongo.db.codedpad
 
-def check_password(password):
-    db_password = coded.find_one({'password' : password })
+def check_password():
+    db_password = coded.find_one({'password' : session['user_password'] })
     return db_password if db_password else False
 
 
@@ -40,10 +43,12 @@ def home():
 @app.route("/notes", methods=['POST'])
 def display_data():
     if request.method == 'POST':
-        session['user_password']= request.form['password']
-        result = check_password(session['user_password'])
-        
-        return render_template('index.html', data=result if result else 'no data') #error if no data use only render index.html 
+        try:
+            session['user_password']= request.form['password']
+            result = check_password()
+            return render_template('index.html', data=result if result else 'no data') #error if no data use only render index.html 
+        except Exception as e:
+            return f"<h1>Internal Server Error</h1>"+e    
     return 'get method'
 
 
@@ -52,12 +57,14 @@ def save_data():
     if request.method =='POST':
         value = request.form['data']
         old_data  =check_newdata()
-        if old_data and old_data != value:
+        if old_data and old_data['data'] != value:
             try:
                 update= coded.find_one_and_update({'password' :session['user_password']}, { '$set':{ 'data': value }}) #session['user_password'] = None
                 return redirect('/')
             except Exception as e:
-                return e+'1'
+                print('error '+e)
+
+                return f"<h1>Internal Server Error</h1>"+e
                 
     
         else:#new data / password 
@@ -65,7 +72,8 @@ def save_data():
                 insert= coded.insert_one({'password' :session['user_password'],'data': value  } )
                 return redirect('/')
             except Exception as e:
-                return e+'2'
+                print('error '+e)
+                return f"<h1>Internal Server Error</h1>"+e
             # store_password.clear
             # session['user_password'] = None
 
@@ -79,11 +87,19 @@ def gemini():
 
 def generate_text(prompt):
     try:
-        genai.configure(api_key="AIzaSyCAbWJC8mopXlEHlH6CSPcTK1X1iTbkFW4") 
-        model = genai.GenerativeModel("gemini-1.5-flash")
-        response = model.generate_content(prompt)
-        print(response.text)
-        return response.text.replace('*', '')  # Remove asterisks directly
+        project_id = "affable-hall-427403-u5"
+        location = "us-central1"
+        vertexai.init(project=project_id, location=location)
+
+        model = GenerativeModel("gemini-1.0-pro")
+        chat = model.start_chat()
+
+        def get_chat_response(chat: ChatSession, prompt: str):
+            response = chat.send_message(prompt)
+            return response.text
+
+        return get_chat_response(chat, prompt).replace('**', '')
+
     except Exception as e:
         print(f"An error occurred: {e}")
         return "Error generating text"
@@ -93,8 +109,8 @@ def chat():
     if request.method == 'POST':
         try:
             a= request.form['prompt']
-            text= generate_text(a)
-            return render_template('gemini.html', use=text)
+            text= generate_text(a).replace('*', '')
+            return render_template('gemini.html', use=text.replace('##',''))
         except  Exception as e:
             return render_template('gemini.html', use=e)
             
@@ -105,6 +121,8 @@ def chat():
 @app.route('/amazonclone')
 def amazon():
     return render_template('amazon.html')
+
+# Initialize Vertex AI (usually done once per app instance)
 
 
 @app.route('/ads.txt')
